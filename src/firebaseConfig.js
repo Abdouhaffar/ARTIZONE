76a6e19d185b4482f12a80c3e9e201a7e7522f80
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2025-11-14 13:15:57 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,1 +1,22 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDHuYlu4xDoaom-neYGQBYf4iITeybdrvI",
+  authDomain: "artizone-e3d9c.firebaseapp.com",
+  projectId: "artizone-e3d9c",
+  storageBucket: "artizone-e3d9c.firebasestorage.app",
+  messagingSenderId: "370751376477",
+  appId: "1:370751376477:web:804fb26c7f24db5f3c51b4",
+  measurementId: "G-YLTXWE4TC4"
+};
+
+// Init
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
