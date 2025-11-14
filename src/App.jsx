c59bb307ba5910f8b63e499a86eaea77c6d41959
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2025-11-14 13:10:24 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Home
 import Home from "./components/Home";
+
+// Artisan
 import RegisterArtisan from "./components/RegisterArtisan";
 import SearchArtisan from "./components/SearchArtisan";
+import ArtisanDetails from "./components/ArtisanDetails";
+import ArtisanLogin from "./components/ArtisanLogin"; // ⬅ تمت إضافته
 
+// VIP
 import VipInfo from "./components/VipInfo";
 import VipPayment from "./components/VipPayment";
 import VipManager from "./components/VipManager";
 
+// Admin
 import AdminLogin from "./components/AdminLogin";
 import AdminDashboard from "./components/AdminDashboard";
-
-import ArtisanDetails from "./components/ArtisanDetails";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Home */}
+        {/* HOME */}
         <Route path="/" element={<Home />} />
 
-        {/* Artisan */}
+        {/* ARTISAN */}
         <Route path="/register" element={<RegisterArtisan />} />
+        <Route path="/artisan-login" element={<ArtisanLogin />} /> {/* ⬅ الجديد */}
         <Route path="/search" element={<SearchArtisan />} />
         <Route path="/artisan/:id" element={<ArtisanDetails />} />
 
