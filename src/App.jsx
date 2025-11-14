24eff5001e2e230fd1c8a9f6bf691f7235639cf8
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2025-11-14 12:15:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,41 @@
 
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import RegisterArtisan from "./components/RegisterArtisan";
+import SearchArtisan from "./components/SearchArtisan";
+
+import VipInfo from "./components/VipInfo";
+import VipPayment from "./components/VipPayment";
+import VipManager from "./components/VipManager";
+
+import AdminLogin from "./components/AdminLogin";
+import AdminDashboard from "./components/AdminDashboard";
+
+import ArtisanDetails from "./components/ArtisanDetails";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Home */}
+        <Route path="/" element={<Home />} />
+
+        {/* Artisan */}
+        <Route path="/register" element={<RegisterArtisan />} />
+        <Route path="/search" element={<SearchArtisan />} />
+        <Route path="/artisan/:id" element={<ArtisanDetails />} />
+
+        {/* VIP */}
+        <Route path="/vip" element={<VipInfo />} />
+        <Route path="/vip-payment" element={<VipPayment />} />
+        <Route path="/vip-manager" element={<VipManager />} />
+
+        {/* ADMIN */}
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
